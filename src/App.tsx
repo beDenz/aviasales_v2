9a0fca52b750b32d5eef5334924ab6e3c9506b4e
beDenz <bedenz@gmail.com>
2019-10-02 22:54:08 +0700

    create fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { aviasalesApi } from "./api/api";
 import "./App.scss";
-import test from "./service/test";
 
 const App: React.FC = () => {
-  return (
+  const [apiState, setApiState] = useState<any>(undefined); // Какой тип возвращаемого значение promise?
+  const [requestApiError, setRequestApiError] = useState<boolean>(false);
 
-  );
+  useEffect((): void => {
+    fetch(aviasalesApi.API_SEARCH_AVIASALES)
+      .then(responsive => responsive.json())
+      .then(responsive =>
+        fetch(aviasalesApi.API_SEARCH_TICKETS + `${responsive.searchId}`)
+          .then(data =>
+            data.status === 200 ? data.json() : setRequestApiError(true)
+          )
+          .then(data => setApiState(data))
+      );
+  }, [requestApiError]);
+
+  if (apiState !== undefined) {
+    console.log(apiState);
+  }
+
+  return <div>Hello</div>;
 };
 
 export default App;
