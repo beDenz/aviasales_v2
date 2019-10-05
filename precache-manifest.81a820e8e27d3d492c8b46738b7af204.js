d593262869c2e0b4d1d5a617ecc8d584becec64e
beDenz <bedenz@gmail.com>
2019-10-05 17:43:55 +0700

    Updates

---
+++ precache-manifest.81a820e8e27d3d492c8b46738b7af204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d8a43c7698c26e42592c05d95d4750",
+    "url": "/aviasales_v2/index.html"
+  },
+  {
+    "revision": "569376fabcb75682f484",
+    "url": "/aviasales_v2/static/css/main.7dae70c7.chunk.css"
+  },
+  {
+    "revision": "e3254b54441649ed456b",
+    "url": "/aviasales_v2/static/js/2.633f3a35.chunk.js"
+  },
+  {
+    "revision": "569376fabcb75682f484",
+    "url": "/aviasales_v2/static/js/main.2380990f.chunk.js"
+  },
+  {
+    "revision": "f1c0303ad42fb568ab12",
+    "url": "/aviasales_v2/static/js/runtime-main.6d6a5103.js"
+  },
+  {
+    "revision": "c5fcf8456b31edcf9d2d565266b78b2d",
+    "url": "/aviasales_v2/static/media/logo.c5fcf845.png"
+  }
+]);
